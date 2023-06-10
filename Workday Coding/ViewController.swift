cdf752722343b44f96b35fe106b4646002efecf1
Miguel97a <miguel971029@gmail.com>
2023-06-10 16:26:52 -0700

    Add tableView and tableViewCell views

--- Workday Coding/ViewController.swift
+++ Workday Coding/ViewController.swift
@@ -7,15 +7,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
+    
+    private var contents : [Content] = []
+    private let contentRepository = ContentNetwork.init()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpTableView()
+        loadContent()
+    }
+    
+    private func loadContent() {
+        contentRepository.fetchContent("", 1) { result in
+                switch result{
+                case .success(let contents):
+                    self.contents = contents
+                    
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                case .failure(let error):
+                    print(error)
+                }
+            }
+        
+    }
+    
+    private func setUpTableView(){
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.register(ContentTableViewCell.self, forCellReuseIdentifier: ContentTableViewCell.identifier)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contents.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ContentTableViewCell.identifier, for: indexPath) as! ContentTableViewCell
+        let content = contents[indexPath.row]
+        cell.setUpCell( content.href, content.title, content.description)
 
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+
+    
 }
 
--- Workday Coding/Views/ContentTableViewCell.swift
+++ Workday Coding/Views/ContentTableViewCell.swift
@@ -10,39 +10,37 @@ import UIKit
 class ContentTableViewCell : UITableViewCell {
     static let identifier = "ContentTableViewCell"
     
-    let image : UIImage = {
-        var image = UIImage()
-        
+    let image : UIImageView = {
+        var image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
         return image
     }()
     
     let titleLabel : UILabel = {
         var label = UILabel()
-        
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     let descriptionLabel : UILabel = {
         var label = UILabel()
-        
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     let dateLabel : UILabel = {
         var label = UILabel()
-        
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
-       
+        contentView.addSubview(image)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(descriptionLabel)
+        contentView.addSubview(dateLabel)
         configureConstraints()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
     
     func setUpCell(_ image: String, _ title: String, _ description: String){
@@ -52,7 +50,29 @@ class ContentTableViewCell : UITableViewCell {
     }
     
     private func configureConstraints(){
+        let titleLabelConstraints = [
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            titleLabel.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor)
+        ]
         
+        let descriptionLabelConstraints = [
+            descriptionLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            descriptionLabel.topAnchor.constraint(equalTo: contentView.topAnchor)
+        ]
+
+        
+        let dateLabelConstraints = [
+            dateLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            dateLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
+            dateLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 20)
+        ]
+        
+        NSLayoutConstraint.activate(descriptionLabelConstraints)
+        NSLayoutConstraint.activate(titleLabelConstraints)
+        NSLayoutConstraint.activate(dateLabelConstraints)
     }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
